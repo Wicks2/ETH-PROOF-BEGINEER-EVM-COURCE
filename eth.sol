9cbb398da243636edc1633c7595b4fccaace0ebe
Wicks2 <138298735+Wicks2@users.noreply.github.com>
2023-08-03 21:43:02 +0530

    Update eth.sol

--- eth.sol
+++ eth.sol
@@ -1,27 +1,46 @@
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+
 contract MyToken {
-    string public tokenName;
-    string public tokenAbbrv;
-    uint public totalSupply;
-    mapping(address => uint) public balances;
 
-    constructor(string memory _name, string memory _abbrv, uint _initialSupply) {
-        tokenName = _name;
-        tokenAbbrv = _abbrv;
-        totalSupply = _initialSupply;
-        balances[msg.sender] = _initialSupply;
+    // public variables here
+    string public tokenName = "thakur";
+    string public tokenAbbvr = "thk";
+    uint public totalSupply = 0;
+
+// burn function
+    function burn(address _address, uint _value) public{
+        require(tokenHolders[_address] > _value, "Cannot burn more than balance tokens");
+        totalSupply-=_value;
+        tokenHolders[_address] -= _value;
     }
 
-    function mint(address _address, uint _value) public {
-        totalSupply += _value;
-        balances[_address] += _value;
+    // mapping variable here
+    mapping(address=>uint) public tokenHolders;
+
+
+    // mint function
+    function mint(address _address, uint _value) public{
+        totalSupply+=_value;
+        tokenHolders[_address] += _value;
     }
 
-    function burn(address _address, uint _value) public {
-        require(balances[_address] >= _value, "Insufficient balance");
-        totalSupply -= _value;
-        balances[_address] -= _value;
-    }
+
+    
+
 }
